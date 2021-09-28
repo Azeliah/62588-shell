45986af199e2d8f96409fe25de1a87a73106b531
Adam Harald Jørgensen <adamharald440@gmail.com>
2021-09-28 10:34:09 +0200

    Implemented basic input reading functionality as a starting point

--- main.c
+++ main.c
@@ -1,7 +1,22 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <string.h>
 
-int main(){
-    printf("This will become a shell...");
+int main(void){
+
+    char input_buffer[128];
+    size_t input_length;
+
+    while(1) {
+       fgets(input_buffer, 128, stdin); 
+
+       input_length = strlen(input_buffer);
+       input_buffer[--input_length] = '\0';
+
+       printf("%s\n", input_buffer);
+
+       if (strcmp(input_buffer, "exit") == 0)
+           break;
+    }
+
     return 0;
 }

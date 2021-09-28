9462de9bd807fd8b7aefeebbdcc1a070dd8c1af2
Adam Harald Jørgensen <adamharald440@gmail.com>
2021-09-28 14:20:49 +0200

    Moved token array generation to its own function and added header file

--- shell.c
+++ shell.c
@@ -4,8 +4,9 @@
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
+#include <stdlib.h>
 
-#define INPUT_BUFFER_LENGTH 128
+#include "shell.h"
 
 int main(void) {
 
@@ -25,23 +26,7 @@ int main(void) {
         size_t input_length = strlen(input_buffer);
         input_buffer[--input_length] = '\0';
 
-        /*
-         * Find the necessary token array size based on number of delimiters in input string.
-         * token_array_size starts at 2 because there needs to be space for the NULL terminator,
-         * and because the last token does not have delimiter.
-         */
-        int token_array_size = 2;
-        for (int i = 0; i < input_length; ++i) {
-            if (input_buffer[i] == ' ')
-                ++token_array_size;
-        }
-
-        // Dynamically allocate array of correct size
-        char *tokens[token_array_size];
-
-        // Initialize all elements in the array to NULL to prevent undefined behavior, in case of fucky input
-        for (int i = 0; i < token_array_size; ++i)
-            tokens[i] = NULL;
+        char **tokens = token_array(input_buffer, input_length, ' ');
 
         // Fill array with tokens from input string
         char *token = strtok(input_buffer, " ");
@@ -68,7 +53,35 @@ int main(void) {
         } else { // If we are in parent process, wait for child process to execute and continue with loop
             wait(NULL);
         }
+
+        // Free tokens array to prevent memory leak
+        free(tokens);
     }
 
     return 0;
+}
+
+char **token_array(const char *input_string, size_t input_length, char delimiter) {
+    if (!input_string || input_length == 0)
+        return NULL;
+
+     /*
+     * Find the necessary token array size based on number of delimiters in input string.
+     * token_array_size starts at 2 because there needs to be space for the NULL terminator,
+     * and because the last token does not have delimiter.
+     */
+    size_t token_array_size = 2;
+    for (int i = 0; i < input_length; ++i) {
+        if (input_string[i] == ' ')
+            ++token_array_size;
+    }
+
+    // Dynamically allocate array of correct size
+    char **tokens = malloc(sizeof(char *) * token_array_size);
+
+    // Initialize all elements in the array to NULL to prevent undefined behavior, in case of fucky input
+    for (int i = 0; i < token_array_size; ++i)
+        tokens[i] = NULL;
+
+    return tokens;
 }
